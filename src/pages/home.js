c697f7fcf2b26301a88561126bcc143bef8f2460
ivongarduno <ivonne_1412@hotmail.com>
2021-06-21 09:33:06 -0500

    slider

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,17 @@
 import React from "react";
+import useFetch from "../hooks/useFetch";
+import SliderMovies from "../components/SliderMovies";
+import { URL_API, API } from "../utils/constants";
 
 export default function Home() {
-  return "Home";
+  const newMovies = useFetch(
+    `${URL_API}/movie/now_playing?api_key=${API}&language=en-US&page=1`
+  );
+
+
+  return (
+    <>
+      <SliderMovies movies={newMovies}/>
+    </>
+  );
 }
---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+export const URL_API = "https://api.themoviedb.org/3";
+export const API = "0823eceb7011a1eccf261d3c910334bb";
